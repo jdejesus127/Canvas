e1f5f401674ab365dabf55f89412a4d21ad2f88a
jdejesus127 <jdejesus@yahoo.com>
2015-07-27 21:09:09 -0500

    second commit

--- application.js
+++ application.js
@@ -1,12 +1,23 @@
-var canvas;
-
-canvas = document.getElementById("board");
+var canvas = document.getElementById("board");
 var drawing = canvas.getContext("2d");
+// this is our ball
+var pos = {x: 10, y: 10, vx: 10, vy: 10, sx: 10, sy: 10};
+var interval;
+
+canvas.width = window.innerWidth - 20;
+canvas.height = window.innerHeight -20;
 
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+drawing.fillRect(pos.x, pos.y, pos.sx, pos.sy);
 
-drawing.fillRect(100, 100, 1000, 1001);
+function loop(){
+    drawing.clearRect(0,0,canvas.width, canvas.height);
+    //Adjust our position by velocity of x
+    pos.x = pos.x + pos.vx;
+    //Adjust our position by velocity of y
+    pos.y = pos.y + pos.vy;
+    drawing.fillRect(pos.x, pos.y, pos.sx, pos.sy);
+    
+}
 
-
+interval = setInterval(loop, 50);
